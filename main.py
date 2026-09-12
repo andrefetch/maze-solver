eb325438ed464e07f57242e27c230857f2247ec1
Andre Nunes da Silva <andresilvapersonal2007@gmail.com>
2026-09-12 18:00:26 -0400

    Added: Dynamic Maze Drawing utilizing 2D arrays

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import time
 from tkinter import Tk, BOTH, Canvas
 
 class Window:
@@ -9,8 +10,9 @@ class Window:
         self.__root = Tk()
         self.__root.title("Maze Solver")
         self.__canvas = Canvas(self.__root)
-        self.__canvas.pack()
+        self.__canvas.pack(fill=BOTH, expand=True)
         self.running = False
+        self.__root.geometry(f"{width}x{height}")
 
     def draw_line(self, line, fill_color) -> None:
 
@@ -59,7 +61,7 @@ class Line:
 
 class Cell():
 
-    def __init__(self, win: Window):
+    def __init__(self, win: Window) -> None:
 
         self.__win = win
         self.has_left_wall = True
@@ -72,12 +74,18 @@ class Cell():
         self.__y1 = -1.0
         self.__y2 = -1.0
 
-    def draw(self, x, y):
+    def draw(
+            self, 
+            x1: float | int,
+            y1: float | int,
+            x2: float | int,
+            y2: float | int,
+        ):
 
-        self.__x1 = x
-        self.__y1 = y
-        self.__x2 = x + 50
-        self.__y2 = y + 50
+        self.__x1 = x1
+        self.__y1 = y1
+        self.__x2 = x2 
+        self.__y2 = y2 
 
         if self.has_left_wall:
             line = Line(
@@ -133,17 +141,79 @@ class Cell():
         else:
             self.__win.draw_line(line, "grey")
 
+class Maze:
+
+    def __init__(
+            
+        self,
+        x1: int,
+        y1: int,
+        num_rows: int,
+        num_cols: int,
+        cell_size_x: float,
+        cell_size_y: float,
+        win: Window,
+
+   ) -> None:
+
+        self.x1 = x1
+        self.y1 = y1
+        self.num_rows = num_rows
+        self.num_cols = num_cols
+        self.cell_size_x = cell_size_x
+        self.cell_size_y = cell_size_y
+        self.win = win
+        self.__cells = []
+
+        self.__create_cells()
+
+    def __create_cells(self):
+
+        for i in range(self.num_cols):
+
+            columns = []
+
+            for j in range(self.num_rows):
+
+                maze_cell = Cell(self.win)
+                columns.append(maze_cell)
+
+            self.__cells.append(columns)
+
+        for i in range(self.num_cols):
+
+            for j in range(self.num_rows):
+
+                self.__draw_cell(i, j)
+
+    def __draw_cell(self, i, j):
+
+        x1 = self.x1 + (i * self.cell_size_x)
+        x2 = x1 + self.cell_size_x
+
+        y1 = self.y1 + (j * self.cell_size_y)
+        y2 = y1 + self.cell_size_y
+
+        self.__cells[i][j].draw(x1, y1, x2, y2)
+        self.__animate()
+
+    def __animate(self):
+        self.win.redraw()
+        time.sleep(0.05)
+
 def main() -> None:
 
-    win = Window(800, 200)
+    win = Window(1280, 720)
 
-    cell_a = Cell(win)
-    cell_a.draw(20, 20)
-
-    cell_b = Cell(win)
-    cell_b.draw(200, 50)
-
-    cell_a.draw_move(cell_b, undo=False)
+    maze = Maze(
+        x1=20,
+        y1=30,
+        num_rows=12,
+        num_cols=24,
+        cell_size_x=50,
+        cell_size_y=50,
+        win=win,
+    )
 
     win.wait_for_close()
 
