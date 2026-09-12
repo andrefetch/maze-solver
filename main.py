eced7e0632c9e189c4edfcd8bb0d873df221aa61
Andre Nunes da Silva <andresilvapersonal2007@gmail.com>
2026-09-12 11:42:05 -0400

    Added: Line Drawing Functionality

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ class Window:
         self.__canvas.pack()
         self.running = False
 
+    def draw_line(self, line, fill_color) -> None:
+        line.draw(self.__canvas, fill_color=fill_color)
+
     def redraw(self) -> None:
         self.__root.update_idletasks()
         self.__root.update()
@@ -23,8 +26,32 @@ class Window:
         self.running = False
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
 
+class Point:
+    def __init__(self, x: float, y: float) -> None:
+        self.x = x;
+        self.y = y;
+
+class Line:
+    def __init__(self, point_one, point_two) -> None:
+        self.point_one = point_one
+        self.point_two = point_two
+
+    def draw(self, canvas, fill_color):
+        canvas.create_line(
+            self.point_one.x, 
+            self.point_one.y, 
+            self.point_two.x, 
+            self.point_two.y, 
+            fill=fill_color, 
+            width=2
+        )
+
 def main() -> None:
     win = Window(800, 600)
+    p1 = Point(100, 20)
+    p2 = Point(30, 40)
+    line = Line(p1, p2)
+    win.draw_line(line, "red")
     win.wait_for_close()
 
 if __name__ == "__main__":
